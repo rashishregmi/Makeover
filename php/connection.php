b038cdb04411e7fa390d69ea762f3402cb543d8f
Rashish regmi <rashish.regmi100@gmail.com>
2023-07-28 14:10:08 +0545

    .

--- php/connection.php
+++ php/connection.php
@@ -1,5 +1,4 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -12,5 +11,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
--- php/signup.php
+++ php/signup.php
@@ -2,45 +2,41 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+var_dump($_POST);
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "makeover";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Debugging: Print the received POST data
-    print_r($_POST);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"])) {
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
-    // Get the user's registration data from the form
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+        // Database query to insert data
+        $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sss", $username, $email, $password);
 
-    // Debugging: Check if data is received correctly
-    echo "Username: " . $username . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Password: " . $password . "<br>";
+        if ($stmt->execute()) {
+            echo "Data stored successfully!";
+        } else {
+            echo "Error: " . $conn->error;
+        }
 
-    // ... (rest of the code for hashing password, preparing SQL query, etc.)
-
-    // Execute the query
-    if ($conn->query($sql) === TRUE) {
-        // Registration successful
-        echo "Registration successful!";
-    } else {
-        // Error occurred during registration
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        $stmt->close();
     }
 }
 
-// Close the database connection
 $conn->close();
 ?>
+
+
